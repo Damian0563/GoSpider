c832d1258fb22aa5dd92d5349f58e078158594c9
Damian0563 <damian.realmadrid2005@gmail.com>
2025-10-09 07:15:01 +0200

    crawl start

--- cmd/worker.go
+++ cmd/worker.go
@@ -1,14 +1,59 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+type Page struct {
+	url     string
+	content string
+}
+
+func (page *Page) Split() []string {
+	return strings.Split(page.content, " ")
+}
+
+func (page *Page) getLinks() {
+	body := page.Split()
+	for _, word := range body {
+		if strings.HasPrefix(word, "href") {
+			fmt.Println(word)
+		}
+	}
+}
 
 var command = &cobra.Command{
 	Use:   "crawl",
 	Short: "Crawl website for redirecting links",
 	Long:  "Crawl website for redirecting links",
-	Run:   crawl,
+	Run: func(cmd *cobra.Command, args []string) {
+		crawl(cmd, args[0])
+	},
+	Args: cobra.ExactArgs(1),
 }
 
-func crawl(cmd *cobra.Command, args []string) {
+func crawl(cmd *cobra.Command, url string) {
+	page := Page{url: url}
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	defer resp.Body.Close()
+	bodyByte, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	page.content = string(bodyByte)
+	page.getLinks()
+}
 
+func init() {
+	rootCmd.AddCommand(command)
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+### GoSpider
+Web crawler written in Go using Cobra cli package that fetches all redirecting links to external websites and organizes them into structured and concise structure.
+
+### Supported commands
+```
+gospider crawl <WEBPAGE_URL>
+```
+
+### Future developments
+* Developing a search engine
