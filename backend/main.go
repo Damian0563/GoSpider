fb023f05c1cd4f9cab26d08f42db9f0a79715b90
Damian0563 <damian.piechocki05@gmail.com>
2025-11-23 19:09:11 +0100

    default vue cleanupp, set up complete

--- backend/main.go
+++ backend/main.go
@@ -3,16 +3,23 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"net/http"
 	"github.com/joho/godotenv"
 )
 
+func homepage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main(){
-	fmt.Println("test")
 	if err := godotenv.Load(); err != nil {
 		log.Println(err)
 		log.Println("No .env file found — using system environment variables")
 	}
-	uri := os.Getenv("MONGO_URI")
-	fmt.Println(uri)
+	// uri := os.Getenv("MONGO_URI")
+	// fmt.Println(uri)
+	http.HandleFunc("/",homepage)
+	log.Fatal(http.ListenAndServe(":8080",nil))
+
+
 }
---
+++ frontend/GoSpider/src/components/Search.vue
@@ -0,0 +1,7 @@
+<template>
+  Search  
+</template>
+
+<style scoped>
+
+</style>
